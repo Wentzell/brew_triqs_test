dc9207fd1e349f4c490579b7fb8bd310d77dc6f2
Nils Wentzell <nils.wentzell@gmail.com>
2017-06-28 01:01:17 -0400

    minor

--- triqs.rb
+++ triqs.rb
@@ -23,8 +23,8 @@ class Triqs < Formula
   depends_on "numpy" => :python
   depends_on "scipy" => :python
   system "pip", "install", "mako"
-  system "pip", "install", "--force-reinstall", "--no-binary=h5py", "h5py"
-  system "pip", "install", "--force-reinstall", "--no-binary=mpi4py", "mpi4py"
+  system "pip", "install", "--no-binary=h5py", "h5py"
+  system "pip", "install", "--no-binary=mpi4py", "mpi4py"
 
   def install
     ENV.cxx11
